0a7fb13a2b5d5a564b484e05849ba9820fc09879
Co30Lc <coralie.leblond@hotmail.com>
2025-04-18 17:22:05 -0400

    This is the first commit with all the files template ready to go

---
+++ 02_Dynamic/index.js
@@ -0,0 +1,27 @@
+
+//Place older for Header.html
+
+//first option bigger
+// document.addEventListener("DOMContentLoaded", function () {
+//     fetch('header.html')
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error("Erreur lors du chargement du header");
+//         }
+//         return response.text();
+//       })
+//       .then(html => {
+//         document.getElementById('cont-header').innerHTML = html;
+//       })
+//       .catch(error => {
+//         console.error("Erreur :", error);
+//       });
+//   });
+  
+document.addEventListener('DOMContentLoaded', function (){
+    document.querySelectorAll('[data-include]').forEach(element => {
+        fetch(element.getAttribute('data-include'))
+        .then(res => res.text())
+        .then(html => element.innerHTML = html);
+    });
+})
